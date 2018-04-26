3d6890e4000c4338a05f8c1837c69e2ad749f8ec
Abel M. Osorio <abel.m.osorio@gmail.com>
2018-04-26 09:02:26 -0300

    Better handling of sinon's stubs on test/simulate_route_dispatch.js.

--- test/simulate_route_dispatch.js
+++ test/simulate_route_dispatch.js
@@ -4,41 +4,50 @@ import { simulateRouteDispatch } from '../src';
 import * as lib from '../src/lib';
 
 describe('simulateRouteDispatch', () => {
+  let stubs = [];
+
+  afterEach('Restore stubs', () => {
+    stubs.forEach((stub) => stub.restore());
+    stubs = [];
+  });
+
   it('should be a function', () => {
     expect(simulateRouteDispatch).to.be.a('function');
   });
 
   it('should call to findRouteLayer once with the right arguments', () => {
-    sinon.stub(lib, 'findRouteLayer').callsFake(() => ({
-      handle: () => true
-    }));
+    stubs.push(
+      sinon.stub(lib, 'findRouteLayer').callsFake(() => ({
+        handle: () => true
+      }))
+    );
 
     simulateRouteDispatch('a', 'b', 'c');
 
     expect(lib.findRouteLayer).to.be.calledOnce;
     expect(lib.findRouteLayer).to.be.calledWith('a', 'b', 'c');
-
-    lib.findRouteLayer.restore();
   });
 
   it('should call to the route\'s handler', () => {
     const handlerSpy = sinon.spy();
 
-    sinon.stub(lib, 'findRouteLayer')
-      .callsFake(() => ({ handle: handlerSpy }));
+    stubs.push(
+      sinon.stub(lib, 'findRouteLayer')
+        .callsFake(() => ({ handle: handlerSpy }))
+    );
 
     simulateRouteDispatch(null, 'get');
 
     expect(handlerSpy).to.be.calledOnce;
-
-    lib.findRouteLayer.restore();
   });
 
   it('should return a stub representing the Response object', () => {
     const handlerSpy = sinon.spy();
 
-    sinon.stub(lib, 'findRouteLayer')
-      .callsFake(() => ({ handle: () => true }));
+    stubs.push(
+      sinon.stub(lib, 'findRouteLayer')
+        .callsFake(() => ({ handle: () => true }))
+    );
 
     const res = simulateRouteDispatch(null, 'get');
 
@@ -47,15 +56,13 @@ describe('simulateRouteDispatch', () => {
     expect(res.append).to.exist;
     expect(res.append.isSinonProxy).to.exist;
     expect(res.append.isSinonProxy).to.be.true;
-
-    lib.findRouteLayer.restore();
   });
 
   it('should throw an exception if the route does not exists', () => {
-    sinon.stub(lib, 'findRouteLayer').callsFake(() => undefined);
+    stubs.push(
+      sinon.stub(lib, 'findRouteLayer').callsFake(() => undefined)
+    );
 
     expect(simulateRouteDispatch).to.throw();
-
-    lib.findRouteLayer.restore();
   });
 });
